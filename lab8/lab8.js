61305143ccdd419d0afe0142694a845b0b583ddb
unknown <maksim.akishin.04@mail.ru>
2024-05-31 12:30:39 +0700

    вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,6 +1,7 @@
 function ShowDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
+    const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
     out.innerHTML = 'Локаль России: ' + today.toLocaleDateString('ru-RU') + '<br>';
     out.innerHTML += 'Локаль Германии: ' + today.toLocaleDateString('de') + '<br>';
     out.innerHTML += 'Локаль Великобритании: ' + today.toLocaleDateString('en-GB') + '<br>';
@@ -9,4 +10,10 @@ function ShowDate() {
     out.innerHTML += 'Локаль Китая: ' + today.toLocaleDateString('zh') + '<br>';
     out.innerHTML += 'Локаль Японии: ' + today.toLocaleDateString('ja-JP') + '<br>';
     out.innerHTML += 'Локаль Италии: ' + today.toLocaleDateString('it-IT') + '<br>';
+    out.innerHTML += '<br>' + 'Текущий год: ' + today.getFullYear() + '<br>';
+    out.innerHTML += 'Текущий месяц: ' + today.getMonth() + '<br>';
+    out.innerHTML += 'Текущая дата: ' + today.getDate() + '<br>';
+    out.innerHTML += 'Текущий день недели (номер): ' + today.getDay() + '<br>';
+    dayNumber = today.getDay();
+    out.innerHTML += 'Текущий день недели: ' + weekDays[dayNumber] + '<br>';
 }
