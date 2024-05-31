4dde2a1064fa48582cdcf6c5e8f9a34c747baad8
unknown <maksim.akishin.04@mail.ru>
2024-06-01 00:01:12 +0700

    Количество дней с даты рождения и добавление ссылки

---
+++ lab8/get-days-count.js
@@ -0,0 +1,9 @@
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    document.getElementById('daysCountResult').innerText = `Количество дней с даты ${inputDate.value}: ${daysCount}`;
+    inputDate.value = '';
+}
