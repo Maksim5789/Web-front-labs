fbe80e0896516723562cff34d008b2d57c5dcb70
unknown <maksim.akishin.04@mail.ru>
2024-06-01 14:55:13 +0700

    Аналоговые часы.2

--- lab8/showTime.js
+++ lab8/showTime.js
@@ -1,22 +1,21 @@
 function showTime() {
     let today = new Date();
-    let currentTime = today.getSeconds();
-    document.getElementById('time').innerHTML = currentTime;
-
-    let displayTime = document.getElementById('timeDisplay');
-    displayTime.innerHTML = currentTime;
-
     let hours = today.getHours();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
+    document.getElementById('time').innerHTML = hours + ':' + minutes + ':' + seconds;
 
+    let displayTime = document.getElementById('timeDisplay');
+    displayTime.innerHTML = seconds;
+    
+    // Делаем так, чтобы время на часах показывало время пребывания на странице
     let hourHand = document.getElementById('hourHand');
-    hourHand.style.transform = `rotate(${30 * hours + minutes / 2 - 90}deg)`;
+    hourHand.style.transform = `rotate(${30 * hours + minutes / 2 - 249}deg)`;
 
     let minuteHand = document.getElementById('minuteHand');
-    minuteHand.style.transform = `rotate(${6 * minutes}deg)`;
+    minuteHand.style.transform = `rotate(${6 * minutes - 293}deg)`;
 
     let secondHand = document.getElementById('secondHand');
-    secondHand.style.transform = `rotate(${6 * seconds}deg)`;
+    secondHand.style.transform = `rotate(${6 * seconds - 180}deg)`;
 }
 setInterval(showTime, 1000);
