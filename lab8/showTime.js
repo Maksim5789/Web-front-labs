5bb38dfb615fb5b98ad5a8f671affe027e1e1e4a
unknown <maksim.akishin.04@mail.ru>
2024-06-01 12:39:15 +0700

    Аналоговые часы

--- lab8/showTime.js
+++ lab8/showTime.js
@@ -5,5 +5,18 @@ function showTime() {
 
     let displayTime = document.getElementById('timeDisplay');
     displayTime.innerHTML = currentTime;
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    let hourHand = document.getElementById('hourHand');
+    hourHand.style.transform = `rotate(${30 * hours + minutes / 2 - 90}deg)`;
+
+    let minuteHand = document.getElementById('minuteHand');
+    minuteHand.style.transform = `rotate(${6 * minutes}deg)`;
+
+    let secondHand = document.getElementById('secondHand');
+    secondHand.style.transform = `rotate(${6 * seconds}deg)`;
 }
 setInterval(showTime, 1000);
